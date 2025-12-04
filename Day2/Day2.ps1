cf023d6f100f3189b8fc936add6b57eff8471ed9
Your Name <you@example.com>
2025-12-03 21:03:40 -0500

    passes examples but isnt fully correct

--- Day2/Day2.ps1
+++ Day2/Day2.ps1
@@ -3,8 +3,9 @@ $inp = Get-Content input.txt
 
 $global:invalidIDS = 0
 
-$ranges = $inp.Split(',')
-#$ranges = @("11-22","95-115","998-1012","1188511880-1188511890","222220-222224","1698522-1698528","446443-446449","38593856-38593862")
+#$ranges = $inp.Split(',')
+$ranges = @("11-22","95-115","998-1012","1188511880-1188511890","222220-222224","1698522-1698528","446443-446449","38593856-38593862", "565653-565659", "824824821-824824827", "2121212118-2121212124")
+#$ranges = @("824824821-824824827")
 foreach($range in $ranges){
 
     $bounds = $range.Split('-')
@@ -13,29 +14,70 @@ foreach($range in $ranges){
         
         $value = [string]$i
     
-        
-        if(($value.Length % 2) -eq 0){
-            
-            if($value.Length -eq 2){
+        if($value.Length -eq 2){
                 
-                [string]$lastHalf = $value[1]
-                
-            }
-            else{
-                
-                $substring = $value.Length /2
-                
-                [string]$lastHalf = [string]$value.Substring($substring)
-                
-            }
-
-            if($value.StartsWith($lastHalf)){
-                
+            if($value[0].Equals($value[1])){
+                $value
                 $global:invalidIDS = $global:invalidIDS + $i
-
             }
-        
+                
         }
+        else{
+                
+            $halfSubstring = $value.Length / 2
+            $a = $value.Length / 3
+            $oneFourthSubstring = $value.Length / 4
+            $oneFifthSubstring = $value.Length / 5
+
+            if($oneFifthSubstring.GetType().name -eq "Int32"){
+                
+                [string]$lastOneFifth = [string]$value.Substring(0,$oneFifthSubstring)
+                
+                if($value -match "($lastOneFifth)\1{4}"){
+                    $global:invalidIDS = $global:invalidIDS + $i
+                    continue
+                }
+            }
+
+            if($oneFourthSubstring.GetType().name -eq "Int32"){
+    
+
+                [string]$lastOneFourth = [string]$value.Substring(0,$oneFourthSubstring)
+                
+                if($value -match "($lastOneFourth)\1{3}"){
+                    #write-host "Last one fourth:" $value
+                    $global:invalidIDS = $global:invalidIDS + $i
+                    continue
+                }
+
+
+            } 
+            
+            if($a.GetType().name -eq "Int32"){
+                 
+                [string]$lastOneThird = [string]$value.Substring(0,$a)
+
+                if($value -match "($lastOneThird)\1{2}"){
+                    #write-host "Last one third:" $value
+                    $global:invalidIDS = $global:invalidIDS + $i
+                    continue
+                        
+                }
+
+            } 
+            
+            if ($halfSubstring.GetType().name -eq "Int32"){
+
+                [string]$lastHalf = [string]$value.Substring($halfSubstring)
+                if($value -match "($lastHalf)\1") {
+                    #write-host "Last half:" $value
+                    $global:invalidIDS = $global:invalidIDS + $i
+                    continue
+                }
+                    
+            }
+                        
+        }           
      
     }
 
